1164a6a53fc8cd0e198f895d91673839826b6c6d
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-31 13:37:48 +0200

    added the smoke shell for the soviet

--- src/app/page.js
+++ src/app/page.js
@@ -93,6 +93,56 @@ const artilleryData = {
                     { range: 2200, mil: 896 },
                     { range: 2300, mil: 820 }
                 ]
+            },
+            "Smoke": {
+                "1": [
+                    { range: 50, mil: 1450 },
+                    { range: 100, mil: 1399 },
+                    { range: 150, mil: 1347 },
+                    { range: 200, mil: 1292 },
+                    { range: 250, mil: 1235 },
+                    { range: 300, mil: 1172 },
+                    { range: 350, mil: 1102 },
+                    { range: 400, mil: 1020 },
+                    { range: 450, mil: 898 }
+                ],
+                "2": [
+                    { range: 200, mil: 1381 },
+                    { range: 300, mil: 1319 },
+                    { range: 400, mil: 1252 },
+                    { range: 500, mil: 1179 },
+                    { range: 600, mil: 1097 },
+                    { range: 700, mil: 993 },
+                    { range: 800, mil: 805 }
+                ],
+                "3": [
+                    { range: 300, mil: 1387 },
+                    { range: 400, mil: 1348 },
+                    { range: 500, mil: 1308 },
+                    { range: 600, mil: 1266 },
+                    { range: 700, mil: 1222 },
+                    { range: 800, mil: 1175 },
+                    { range: 900, mil: 1123 },
+                    { range: 1000, mil: 1065 },
+                    { range: 1100, mil: 994 },
+                    { range: 1200, mil: 902 }
+                ],
+                "4": [
+                    { range: 400, mil: 1387 },
+                    { range: 500, mil: 1357 },
+                    { range: 600, mil: 1327 },
+                    { range: 700, mil: 1286 },
+                    { range: 800, mil: 1264 },
+                    { range: 900, mil: 1231 },
+                    { range: 1000, mil: 1196 },
+                    { range: 1100, mil: 1159 },
+                    { range: 1200, mil: 1119 },
+                    { range: 1300, mil: 1075 },
+                    { range: 1400, mil: 1026 },
+                    { range: 1500, mil: 969 },
+                    { range: 1600, mil: 896 },
+                    { range: 1700, mil: 753 }
+                ]
             }
         }
     }
