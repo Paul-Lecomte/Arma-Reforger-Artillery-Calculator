0f765f543925b2e5ed5d59ab77486e97deb34e9e
Paul <lecomtepaulvd@gmail.com>
2025-03-31 09:43:07 +0200

    basic structure

---
+++ src/app/components/Math.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Math = (props) => (
+    
+);
+
+export default Math;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -22,6 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Header></Header>
+      <Footer></Footer>
         {children}
       </body>
     </html>
