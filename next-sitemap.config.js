3d79e324d87f80aea0d8cfa8f1dab0b5f680b5fb
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-02 08:58:13 +0200

    sitemap update

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: "https://arma-reforger-artillery-calculator.vercel.app", // Update this if using a custom domain
-    generateRobotsTxt: true,
-    sitemapSize: 5000, // Prevents splitting into multiple files
-};
