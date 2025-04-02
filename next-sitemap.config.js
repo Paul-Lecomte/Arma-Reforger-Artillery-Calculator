06c914c66694f4e1ea41f7f2910f2f0a5087824b
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-02 08:36:48 +0200

    build robots.txt sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: "https://arma-reforger-artillery-calculator.vercel.app", // Change to your actual domain
+    generateRobotsTxt: true, // Generates robots.txt
+};
--- next-sitemap.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://arma-reforger-artillery-calculator.vercel.app',
-    generateRobotsTxt: true,
-};
