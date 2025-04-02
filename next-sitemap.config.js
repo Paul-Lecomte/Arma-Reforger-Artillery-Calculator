97fbee24c3e509661745d5ff9a9c10e893199e6b
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-02 08:45:44 +0200

    manual sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://arma-reforger-artillery-calculator.vercel.app", // Change to your actual domain
-    generateRobotsTxt: true, // Generates robots.txt
+    siteUrl: "https://arma-reforger-artillery-calculator.vercel.app", // Update this if using a custom domain
+    generateRobotsTxt: true,
+    sitemapSize: 5000, // Prevents splitting into multiple files
 };
