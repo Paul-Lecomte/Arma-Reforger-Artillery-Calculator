fea7d0d5067e53bb5f2c845bfead3d51c06d2405
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-02 09:32:56 +0200

    updated the footer

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,7 +1,13 @@
 const Footer = () => {
     return (
         <footer className="text-gray-400 text-xs text-center bottom-0 w-full">
-            <p>&copy; {new Date().getFullYear()} • MIT License •
+            <p>&copy; {new Date().getFullYear()} Paul Lecomte • MIT License •
+                <a href="https://github.com/Paul-Lecomte/Arma-Reforger-Artillery-Calculator"
+                   target="_blank"
+                   rel="noopener noreferrer"
+                   className="hover:text-white ml-1">
+                    GitHub
+                </a>
             </p>
         </footer>
     );
