6696ceafcf572a0bea8547c5cf110a05a100e098
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-04-05 09:47:16 +0200

    improving the header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
 
     return (
         <header className="text-white py-4 bg-black z-50">
-            <div className="container flex px-6" style={{marginLeft: "5rem"}}>
+            <div className="container flex px-6" style={{ marginLeft: "5rem" }}>
                 <h1 className="text-2xl font-bold tracking-wide hidden md:block transition-opacity duration-300">
                     Arma Reforger Artillery Calculator
                 </h1>
@@ -23,12 +23,16 @@ const Header = () => {
 
                 {/* Navigation Links */}
                 <nav
-                    className={`absolute md:static top-12 right-0 md:w-auto bg-gray-800 md:bg-transparent transition-all duration-300 ease-in-out transform md:transform-none ${
-                        isOpen ? "opacity-100 translate-y-0 scale-100 pointer-events-auto" : "opacity-0 -translate-y-4 scale-95 pointer-events-none"
-                    } md:opacity-100 md:translate-y-0 md:scale-100 md:pointer-events-auto md:flex md:space-x-6`}
-                    style={{zIndex: 40}}
+                    className={`absolute md:static top-12 right-0 md:w-auto transition-all duration-300 ${
+                        isOpen ? "block" : "hidden"
+                    } md:flex md:space-x-6`}
+                    style={{ zIndex: 40 }}
                 >
-                    <ul className="flex flex-col bg-gray-800 rounded-lg absolute top-5 right-4 items-end md:flex-row space-y-4 md:space-y-0 md:space-x-6 p-6 md:p-0 ">
+                    <ul
+                        className={`flex flex-col items-end md:flex-row space-y-4 md:space-y-0 md:space-x-6 p-6 md:p-0 rounded-lg absolute top-5 right-4 ${
+                            isOpen ? "bg-gray-800" : ""
+                        } md:bg-transparent`}
+                    >
                         <li>
                             <Link href="/" className="hover:text-gray-400" onClick={() => setIsOpen(false)}>
                                 Map
