f3d6fb0510258bb24aac7a9004f32c800bf8340d
Paul <lecomtepaulvd@gmail.com>
2025-07-28 08:42:36 +0200

    comment

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import dynamic from "next/dynamic";
 
+// Importing the MapCalculator component dynamically to avoid server-side rendering issues
 const MapCalculatorPage = dynamic(() => import("./components/MapCalculator"), { ssr: false });
 
 export default function Page() {
